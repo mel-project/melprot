91c8353b12cfb50583cfd8bd84938c0ae6bf5f22
nullchinchilla <nullchinchilla@pm.me>
2022-01-13 13:55:12 -0600

    up

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use std::{collections::BTreeMap, marker::PhantomData, sync::Arc};
 
+use async_trait::async_trait;
 use melnet::Request;
 use novasmt::{CompressedProof, ContentAddrStore};
 use themelio_stf::{AbbrBlock, BlockHeight, ConsensusProof, SealedState, Transaction};
@@ -10,18 +11,16 @@ use crate::{NodeRequest, StateSummary, Substate};
 /// This trait represents a server of Themelio's node protocol. Actual nodes should implement this.
 pub trait NodeServer<C: ContentAddrStore>: Send + Sync {
     /// Broadcasts a transaction to the network
-    fn send_tx(&self, state: melnet::NetState, tx: Transaction) -> melnet::Result<()>;
+    fn send_tx(&self, state: melnet::NetState, tx: Transaction) -> anyhow::Result<()>;
 
     /// Gets an "abbreviated block"
-    fn get_abbr_block(&self, height: BlockHeight) -> melnet::Result<(AbbrBlock, ConsensusProof)>;
-
-    /// Gets abbreviated blocks within a large rang
+    fn get_abbr_block(&self, height: BlockHeight) -> anyhow::Result<(AbbrBlock, ConsensusProof)>;
 
     /// Gets a state summary
-    fn get_summary(&self) -> melnet::Result<StateSummary>;
+    fn get_summary(&self) -> anyhow::Result<StateSummary>;
 
     /// Gets a full state
-    fn get_state(&self, height: BlockHeight) -> melnet::Result<SealedState<C>>;
+    fn get_state(&self, height: BlockHeight) -> anyhow::Result<SealedState<C>>;
 
     /// Gets an SMT branch
     fn get_smt_branch(
@@ -29,10 +28,10 @@ pub trait NodeServer<C: ContentAddrStore>: Send + Sync {
         height: BlockHeight,
         elem: Substate,
         key: HashVal,
-    ) -> melnet::Result<(Vec<u8>, CompressedProof)>;
+    ) -> anyhow::Result<(Vec<u8>, CompressedProof)>;
 
     /// Gets stakers
-    fn get_stakers_raw(&self, height: BlockHeight) -> melnet::Result<BTreeMap<HashVal, Vec<u8>>>;
+    fn get_stakers_raw(&self, height: BlockHeight) -> anyhow::Result<BTreeMap<HashVal, Vec<u8>>>;
 }
 
 /// This is a melnet responder that wraps a NodeServer.
@@ -51,47 +50,47 @@ impl<C: ContentAddrStore, S: NodeServer<C>> NodeResponder<C, S> {
     }
 }
 
+impl<C: ContentAddrStore, S: NodeServer<C>> Clone for NodeResponder<C, S> {
+    fn clone(&self) -> Self {
+        Self {
+            server: self.server.clone(),
+            _p: Default::default(),
+        }
+    }
+}
+
+#[async_trait]
 impl<C: ContentAddrStore, S: NodeServer<C>> melnet::Endpoint<NodeRequest, Vec<u8>>
     for NodeResponder<C, S>
 {
-    fn respond(&self, req: Request<NodeRequest, Vec<u8>>) {
+    async fn respond(&self, req: Request<NodeRequest>) -> anyhow::Result<Vec<u8>> {
         let state = req.state.clone();
-        match req.body.clone() {
-            NodeRequest::SendTx(tx) => req
-                .response
-                .send(self.server.send_tx(state, tx).map(|_| Vec::new())),
-            NodeRequest::GetSummary => req.response.send(
-                self.server
-                    .get_summary()
-                    .map(|sum| stdcode::serialize(&sum).unwrap()),
-            ),
-            NodeRequest::GetAbbrBlock(height) => req.response.send(
-                self.server
-                    .get_abbr_block(height)
-                    .map(|blk| stdcode::serialize(&blk).unwrap()),
-            ),
-            NodeRequest::GetSmtBranch(height, elem, key) => req.response.send(
-                self.server
-                    .get_smt_branch(height, elem, key)
-                    .map(|v| stdcode::serialize(&v).unwrap()),
-            ),
-            NodeRequest::GetStakersRaw(height) => req.response.send(
-                self.server
-                    .get_stakers_raw(height)
-                    .map(|v| stdcode::serialize(&v).unwrap()),
-            ),
+        let server = self.server.clone();
+        smol::unblock(move || match req.body.clone() {
+            NodeRequest::SendTx(tx) => {
+                server.send_tx(state, tx)?;
+                Ok(vec![])
+            }
+            NodeRequest::GetSummary => Ok(stdcode::serialize(&server.get_summary()?)?),
+            NodeRequest::GetAbbrBlock(height) => {
+                Ok(stdcode::serialize(&server.get_abbr_block(height)?)?)
+            }
+            NodeRequest::GetSmtBranch(height, elem, key) => Ok(stdcode::serialize(
+                &server.get_smt_branch(height, elem, key)?,
+            )?),
+            NodeRequest::GetStakersRaw(height) => {
+                Ok(stdcode::serialize(&server.get_stakers_raw(height)?)?)
+            }
             NodeRequest::GetPartialBlock(height, mut hvv) => {
-                let server = self.server.clone();
                 hvv.sort();
                 let hvv = hvv;
-                let res = server.get_state(height).map(|ss| {
-                    let mut blk = ss.to_block();
-                    blk.transactions
-                        .retain(|h| hvv.binary_search(&h.hash_nosigs()).is_ok());
-                    stdcode::serialize(&blk).unwrap()
-                });
-                req.response.send(res);
+                let ss = server.get_state(height)?;
+                let mut blk = ss.to_block();
+                blk.transactions
+                    .retain(|h| hvv.binary_search(&h.hash_nosigs()).is_ok());
+                Ok(stdcode::serialize(&blk)?)
             }
-        }
+        })
+        .await
     }
 }
